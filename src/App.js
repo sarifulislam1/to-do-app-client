772e8a3dfc20d5ca917e0b69a6c1b5e5d27c3883
Your Name <sarifulislaml8280@gmail.com>
2022-05-18 22:02:17 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './component/NavBar/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './component/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      {/* <Home></Home>s */}
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
